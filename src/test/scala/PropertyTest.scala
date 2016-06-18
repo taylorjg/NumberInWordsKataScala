ed63c0dbbabc17636553478b9a8fc54dd762e1fe
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-06-18 18:47:46 +0100

    Simplified the property test a bit more

--- src/test/scala/PropertyTest.scala
+++ src/test/scala/PropertyTest.scala
@@ -4,10 +4,6 @@ import org.scalacheck.Gen.choose
 import org.scalacheck.Prop.forAll
 
 object PropertyTest extends Properties("Kata") {
-
     val gen = choose(1, 999)
-
-    property("roundTrip") = forAll(gen) { n: Int =>
-        wordsToNumber(numberToWords(n)) == n
-    }
+    property("roundTrip") = forAll(gen) { n => wordsToNumber(numberToWords(n)) == n }
 }
